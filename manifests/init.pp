de2d1604b05e3820ed3ad7aeaf2729e587b3842e
SATERDAL Tore <tsaterdal@one.steria.dom>
2019-07-24 12:21:14 +0200

    added class for unbound dns

---
+++ manifests/init.pp
@@ -0,0 +1,47 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include unbound
+class unbound {
+
+  File {
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['unbound'],
+    notify  => [Service['unbound'], Exec['unbound-checkconf']],
+  }
+
+  package { 'unbound':
+    ensure   => 'present',
+  }
+
+  file { '/etc/unbound/unbound.conf':
+    content => template('unbound/unbound.conf.epp'),
+  }
+
+  file { '/etc/unbound/root.hints':
+    source  => 'puppet:///modules/unbound/root.hints',
+  }
+
+  service {'unbound':
+    ensure  => 'running',
+    require => Package['unbound'],
+  }
+
+  exec {'unbound setup':
+    command => '/usr/sbin/unbound-control-setup',
+    creates => '/etc/unbound/unbound_server.pem',
+    notify  => Service['unbound'],
+
+  }
+  exec {'unbound-checkconf':
+    before      => Service['unbound'],
+    path        => ['/usr/sbin'],
+    refreshonly => true,
+
+
+  }
+}
